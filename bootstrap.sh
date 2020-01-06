5d933374a9ebdb7b18444bfd0e7ec21c48597581
Luca Cotti <lucacotti@outlook.com>
2020-01-07 00:09:35 +0100

    Update logging and yay installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,35 +28,48 @@ readonly username="Luca Cotti"
 readonly email="lucacotti@outlook.com"
 readonly dotfiles_repo="https://github.com/LucaCtt/dotfiles"
 readonly dotfiles_dir="${HOME}/.dotfiles/"
+readonly dotfiles_ignore=("${HOME}/README.md" "${HOME}/LICENSE")
 readonly pkgs_basic=(git vim zsh)
 readonly tmp=$(mktemp -d -t -q "bootstrap.XXXXXXXXXX")
 readonly installation_type="$1"
 
 readonly color_light_blue='\033[1;34m'
+readonly color_red='\033[0;31m'
 readonly color_nc='\033[0m' 
 
-cleanup() {
-    if [ -d $tmp ]  
-    then
-        rm -r "$tmp"
-    fi
-}
-
+# Logs the arguments to stdout, using some formatting.
+# ARGS:
+#   $@: The values to log.
 log() {
-    echo -e "${color_light_blue}>${1}${color_nc}"
+    echo -e "${color_light_blue}> ${@}${color_nc}"
 }
 
+# Logs the arguments to stderr with some formatting, then exits with a non-zero code.
+# ARGS:
+#   $#: The values to log.
 err() {
-    echo "$1"
+    echo -e "${color_red}${@}${color_nc}" >&2
     exit 1
 }
 
+# Alias for git, to use for dotfiles.
 dot() {
     git --git-dir="$dotfiles_dir" --work-tree="$HOME" "$@"
 }
 
+# Removes temp directory.
+cleanup() {
+    log "Cleaning up..."
+    if [ -d $tmp ]  
+    then
+        /bin/rm -rf "$tmp"
+    fi
+}
+
+# Executes basic installation.
 basic() {
     local yay_dir="${tmp}/yay"
+
     log "Installing basic packages..."
     sudo pacman -Syuq --noconfirm --needed "${pkgs_basic[@]}"
 
@@ -64,18 +77,22 @@ basic() {
     git config --global user.name "$username"
     git config --global user.email "$email"
 
-    log "Installing yay..."
-    git clone --quiet https://aur.archlinux.org/yay.git "$yay_dir"
-    (
-        cd "$yay_dir"
-        makepkg -si --needed --clean --noconfirm 
-    )
+    
+    if [ ! -x "$(command -v yay)"]
+    then
+        log "Installing yay..."
+        git clone --quiet https://aur.archlinux.org/yay.git "$yay_dir"
+        (
+            cd "$yay_dir"
+            makepkg -si --needed --clean --noconfirm 
+        )
+    fi
 
     log "Installing dotfiles from $dotfiles_repo..."
     git init --quiet --bare "$dotfiles_dir"
     dot config status.showUntrackedFiles no
     dot remote add origin "$dotfiles_repo"
-    dot update-index --assume-unchanged README.md LICENSE
+    dot update-index --assume-unchanged "${dotfiles_ignore[@]}"
     dot pull origin master
 }
 
